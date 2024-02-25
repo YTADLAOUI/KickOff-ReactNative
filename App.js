f93907f185fa6a42ef44a7a24f0f20b10deae979
youssef tadlaoui <usftadlaoui@gmail.com>
2024-02-25 23:12:24 +0100

    optimize tapBar and change icon

--- App.js
+++ App.js
@@ -17,87 +17,67 @@ export default function App() {
   return (
     <Provider store={store}> 
       <NavigationContainer >
-      {/* <Stack.Navigator >
+      <Stack.Navigator >
+          <Stack.Screen
+            name="tapBar"
+            component={tapBar}
+            options={{ headerShown: false }}
+            />
           <Stack.Screen
             name="Details"
             component={MatchDetailsScreen}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Profile"
             component={ProfilePlayer}
+            options={{ headerShown: false }}
           />
-        </Stack.Navigator> */}
-            <Tab.Navigator
-              initialRouteName="Matche"
-              activeColor="#e91e63"
-              barStyle={{ backgroundColor: 'white' }}
-            >
-          <Tab.Screen
-            name="Matche"
-            component={Matche}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="Details"
-            component={MatchDetailsScreen}
-            options={{
-              tabBarLabel: 'Details',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="information" color={color} size={26} />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="Profile"
-            component={ProfilePlayer}
-          />
-          <Tab.Screen
-              name="Player"
-              component={PlayersScreen}
-              options={{
-                tabBarLabel: 'player',
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons name="player" color={color} size={26} />
-                ),
-              }}
-            />
-          {/* <Tab.Screen
-              name="Details"
-              component={MatchDetailsScreen}
-              options={{
-                tabBarLabel: 'Details',
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons name="Details" color={color} size={26} />
-                ),
-              }}
-            />
-          <Tab.Screen
-              name="Profile"
-              component={ProfilePlayer}
-              options={{
-                tabBarLabel: 'Profile',
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons name="profile" color={color} size={26} />
-                ),
-              }}
-            /> */}
-          <Tab.Screen
-              name="Favorite"
-              component={FavoritesScreen}
-              options={{
-                tabBarLabel: 'Favorite',
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons name="Favorite" color={color} size={26} />
-                ),
-              }}
-            />
-        </Tab.Navigator>
+
+        </Stack.Navigator>
+           
       </NavigationContainer>
     </Provider>
   );
+}
+
+const tapBar=()=>{
+  return(
+    <Tab.Navigator
+    initialRouteName="Matche"
+    activeColor="#e91e63"
+    barStyle={{ backgroundColor: 'white' }}
+  >
+<Tab.Screen
+  name="Matche"
+  component={Matche}
+  options={{
+    tabBarLabel: 'Matches',
+    tabBarIcon: ({ color }) => (
+      <MaterialCommunityIcons name="television-classic" color={color} size={26} />
+    ),
+  }}
+/>
+<Tab.Screen
+    name="Player"
+    component={PlayersScreen}
+    options={{
+      tabBarLabel: 'players',
+      tabBarIcon: ({ color }) => (
+        <MaterialCommunityIcons name="account-supervisor-outline" color={color} size={26} />
+      ),
+    }}
+  />
+<Tab.Screen
+    name="Favorite"
+    component={FavoritesScreen}
+    options={{
+      tabBarLabel: 'Favorite',
+      tabBarIcon: ({ color }) => (
+        <MaterialCommunityIcons name="heart" color={color} size={26} />
+      ),
+    }}
+  />
+</Tab.Navigator>
+  )
 }
